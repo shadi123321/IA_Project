08709fb598de23d771afb4c474595a705e404c28
Owaisalbarsha <owaisalbarsha828@gmail.com>
2026-01-09 23:20:21 +0300

    show complaint endpoint

--- routes/api.php
+++ routes/api.php
@@ -52,7 +52,7 @@ Route::get('/resendCode/{id}', [AuthController::class, 'resendCode']);
         Route::get('/governments', [AdminController::class, 'indexGovernment']);
         Route::get('/complaints', [UserController::class, 'complaints']);
         Route::post('/changeStatus', [UserController::class, 'changeStatus']);
-        Route::get('/showComplaint/{reference_number}', [UserController::class, 'showComplaint']);
+        Route::get('/admin/showComplaint/{reference_number}', [UserController::class, 'showComplaint']);
         Route::get('MonitoringComplains', [AdminController::class, 'MonitoringComplains']);
         Route::get('/statistics', [AdminController::class, 'statistics']);
 
@@ -76,7 +76,7 @@ Route::get('/resendCode/{id}', [AuthController::class, 'resendCode']);
         Route::get('/indexByEntity', [UserController::class, 'indexByEntity']);
         Route::post('/EmployeeAddNote', [UserController::class, 'EmployeeAddNote']);
 
-        //Route::put('/changeStatus', [UserController::class, 'changeStatus']);
+        Route::put('/changeStatus', [UserController::class, 'changeStatus']);
         Route::get('/showComplaint/{reference_number}', [UserController::class, 'showComplaint']);
 
     });
